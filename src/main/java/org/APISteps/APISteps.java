35b45c46e95efa7aabd10d00739404b3a0cce5b9
alpiysl <alpiysll@gmail.com>
2025-10-02 01:45:40 +0300

    1st execution test completed

--- src/main/java/org/APISteps/APISteps.java
+++ src/main/java/org/APISteps/APISteps.java
@@ -1,4 +1,23 @@
 package org.APISteps;
 
-public class APISteps {
+import org.Endpoints.Endpoints;
+
+import static io.restassured.RestAssured.given;
+
+public class APISteps implements Endpoints {
+
+    protected final static String BASE_URI;
+    static {
+        BASE_URI = "https://fakerestapi.azurewebsites.net";
+    }
+
+    public static void getActivities() {
+            given()
+                .baseUri(BASE_URI)
+                .get(GET_ACTIVITIES)
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
 }
---
+++ src/test/java/testAuthors.java
@@ -0,0 +1,10 @@
+import org.APISteps.APISteps;
+import org.testng.annotations.Test;
+
+public class testAuthors {
+
+    @Test
+    public void test1(){
+        APISteps.getActivities();
+    }
+}
